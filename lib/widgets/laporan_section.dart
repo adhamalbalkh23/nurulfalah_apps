ca734a223924d1a7f46836dd2773b2b081555fe3
adhamalbalkh23 <adhamdermawan8@gmail.com>
2026-02-26 08:24:36 +0700

    Nurul Falah App

---
+++ lib/widgets/laporan_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LaporanSection extends StatelessWidget {
+  const LaporanSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
